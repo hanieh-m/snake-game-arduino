cd0698f677cb495b4330aece7cd1df9a82751df4
Hanieh Marvi <hanieh.marvi1@gmail.com>
2019-04-24 15:45:53 +0430

    Implement snake movement

--- game.ino
+++ game.ino
@@ -1,4 +1,6 @@
 int incomingByte = 0;   // for incoming serial data
+long snake[64][2] = {{62, 64}, {63, 64}, {64, 64}, {65, 64}};
+long snakeLen = 4;
 
 void setup() {
         Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
@@ -7,18 +9,109 @@ void setup() {
 void loop() {
         unsigned long previousMillis = millis();
         bool moved = false;
+        int dir = 0;
         while(millis() - previousMillis <= 5000) {
+          dir = 0;
           if (Serial.available() > 0) {
                   incomingByte = Serial.read();
-                  Serial.print(incomingByte);
                   moved = true;
+                  dir = incomingByte;
                   break;
           }
         }
-        if(!moved)
-          Serial.print("outside of loop");
+        snakeMove(dir);
 }
 
-void snakeMove(int dir, long snake[][2]) {
+void snakeMove(int dir) {
+  if(dir != 10) {
+    Serial.print("direction: ");
+    Serial.print(dir);
+    Serial.print("    ");
+    int verDir = snake[snakeLen - 1][0] - snake[snakeLen - 2][0];
+    int horDir = snake[snakeLen - 1][1] - snake[snakeLen - 2][1];
+    if( (horDir == 1 && dir == 117) || (horDir == -1 && dir == 100) || (verDir == 1 && dir == -114) || (verDir == 1 && dir == 108) )
+      return;
+    int i = 0;
+    for(i = 0; i < snakeLen - 1; ++i) { // remove first element (the tail of the snake)
+      snake[i][0] = snake[i + 1][0];
+      snake[i][1] = snake[i + 1][1];
+    }
+    switch(dir) {
+      case(0): // continue same movement
+      {
+        if(verDir == 1) { // it is going right
+          snake[snakeLen - 1][0] = snake[snakeLen - 2][0] + 1;
+          snake[snakeLen - 1][1] = snake[snakeLen - 2][1];
+        }
+        else if(verDir == -1) { // it is going left
+          snake[snakeLen - 1][0] = snake[snakeLen - 2][0] - 1;
+          snake[snakeLen - 1][1] = snake[snakeLen - 2][1];
+        }
+        else if(horDir == 1) { // it is going down
+          snake[snakeLen - 1][0] = snake[snakeLen - 2][0];
+          snake[snakeLen - 1][1] = snake[snakeLen - 2][1] + 1;
+        }
+        else if(horDir == -1) { // it is going up
+          snake[snakeLen - 1][0] = snake[snakeLen - 2][0];
+          snake[snakeLen - 1][1] = snake[snakeLen - 2][1] - 1;
+        }
+        break;
+      }
+      case(117): // up
+      {
+        if(horDir != 1) {
+          snake[snakeLen - 1][0] = snake[snakeLen - 2][0];
+          snake[snakeLen - 1][1] = snake[snakeLen - 2][1] - 1;
+        }
+        else {
+          snakeMove(0);
+        }
+        break;
+      }
+      case(100): // down
+      {
+        if(horDir != -1) {
+          snake[snakeLen - 1][0] = snake[snakeLen - 2][0];
+          snake[snakeLen - 1][1] = snake[snakeLen - 2][1] + 1;
+        }
+        else {
+          snakeMove(0);
+        }
+        break;
+      }
+      case(114): // right
+      {
+        if(verDir != -1) {
+          snake[snakeLen - 1][0] = snake[snakeLen - 2][0] + 1;
+          snake[snakeLen - 1][1] = snake[snakeLen - 2][1];
+        }
+        else {
+          snakeMove(0);
+        }
+        break;
+      }
+      case(108): // left
+      {
+        if(verDir != 1) {
+          snake[snakeLen - 1][0] = snake[snakeLen - 2][0] - 1;
+          snake[snakeLen - 1][1] = snake[snakeLen - 2][1];
+        }
+        else {
+          snakeMove(0);
+        }
+        break;
+      }
+    }
+    Serial.print("{ ");
+    for(i = 0; i < snakeLen; ++i){
+       Serial.print("{");
+       Serial.print(snake[i][0]);
+       Serial.print(", ");
+       Serial.print(snake[i][1]);
+       Serial.print("} ");
+    }
+    Serial.print("}");
+    Serial.println();
+  }
   return;
 }
