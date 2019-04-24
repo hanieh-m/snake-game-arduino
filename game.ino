4bc9a0557ddb99a85498ad2c14db0509618d0fd5
Hanieh Marvi <hanieh.marvi1@gmail.com>
2019-04-24 13:50:16 +0430

    Implement simple input and output

--- game.ino
+++ game.ino
@@ -1,8 +1,24 @@
+int incomingByte = 0;   // for incoming serial data
 
 void setup() {
-  
+        Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
 }
 
 void loop() {
-  
+        unsigned long previousMillis = millis();
+        bool moved = false;
+        while(millis() - previousMillis <= 5000) {
+          if (Serial.available() > 0) {
+                  incomingByte = Serial.read();
+                  Serial.print(incomingByte);
+                  moved = true;
+                  break;
+          }
+        }
+        if(!moved)
+          Serial.print("outside of loop");
+}
+
+void snakeMove(int dir, long snake[][2]) {
+  return;
 }
